50c07b07f7dafe07a33b73b8bfe8ba998c33b1ce
Mathieu Nativel <mathieu.nativel@lse.epita.fr>
2020-02-19 18:28:44 +0100

    namespace: add mounting of /proc

--- src/container.c
+++ src/container.c
@@ -1,20 +1,16 @@
 #define _GNU_SOURCE
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/mount.h>
 
 #include "include/container.h"
 #include "include/debug.h"
 
-int container_function(void *raw_arg) {
-    setvbuf(stdout, NULL, _IONBF, 0);
-    struct container_arg *arg = (struct container_arg *) raw_arg;
-    if (!arg->argv[0]) {
-        puts("Usage: ./ccon <cmd>");
-        return 1;
-    }
+#define SETENV_OVERWRITE 1
 
-    LOG("Entering child function.");
-    //LOG("Setting the container hostname to : %s.", arg->opt->hostname);
+
+int set_container_env(struct container_arg *arg) {
     if (sethostname(arg->opt->hostname, strlen(arg->opt->hostname)) < 0) {
         LOGERR("sethostname failed");
         return 1;
@@ -22,6 +18,47 @@ int container_function(void *raw_arg) {
 
     if (chroot(arg->opt->rootfs) == -1) {
         LOGERR("chroot failed");
+        return 1;
+    }
+
+    if (chdir("/") == -1) {
+        LOGERR("chdir failed");
+        return 1;
+    }
+
+    if (setenv("PS1", "cconsh$ ", SETENV_OVERWRITE) == -1) {
+        LOGERR("setenv failed");
+        return 1;
+    }
+
+    return 0;
+}
+
+int set_container_fs() {
+    if (mount("/proc", "/proc", "proc", 0, "") == -1) {
+        LOGERR("mounting proc failed");
+        return 1;
+    }
+
+    return 0;
+}
+
+int container_function(void *raw_arg) {
+    LOG("Entering child function.");
+
+    struct container_arg *arg = (struct container_arg *) raw_arg;
+    if (!arg->argv[0]) {
+        puts("Usage: ./ccon <cmd>");
+        return 1;
+    }
+
+    //LOG("Setting the container hostname to : %s.", arg->opt->hostname);
+    if (set_container_env(arg) != 0) {
+        LOGERR("an error occured in the container environment setup");
+        return 1;
+    }
+
+    if (set_container_fs() != 0) {
         return 1;
     }
 
